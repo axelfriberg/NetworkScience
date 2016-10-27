f96434d22007a16d6c697b7a15da218409651026
HaveMersie <mljmersie@gmail.com>
2016-10-27 22:13:15 +0100

    Analysis of dataset
    Still working on showing the degree distribution using networkx (will
    put the information in latex tomorrow morning)

--- tutorial.py
+++ tutorial.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+import collections
 
 G=nx.Graph()
 
@@ -31,5 +33,41 @@ G.graph["Title"] = "Tutorial"
 
 mygraph = nx.read_gml("karate.gml")
 
+#analyzing dataset
+
 print(nx.clustering(mygraph))
 print(nx.average_clustering(mygraph))
+#Betweenness centrality
+print(nx.betweenness_centrality(mygraph))
+#Closeness centrality
+print(nx.closeness_centrality(mygraph))
+#Eigenvector centrality
+print(nx.eigenvector_centrality(mygraph))
+#degree centrality
+print(nx.degree_centrality(mygraph))
+#dispersion of graph
+print(nx.dispersion(mygraph))
+
+
+# calculating average degree
+
+N,K = mygraph.order(), mygraph.size()
+avg_deg = float(K)/N
+print("Nodes: ", N)
+print("Edges: ", K)
+print("Average degree: ", avg_deg)
+
+#histogram of degree distribution (not done yet)
+
+#degrees = mygraph.degree()
+#values = sorted(set(degrees.values()))
+#hist = []
+
+#plt.figure()
+#plt.plot(degrees, hist, 'ro')
+#plt.legend(['Degree'])
+#plt.xlabel('Degree')
+#plt.ylabel('Number of nodes')
+#plt.title("Zachary's Karate Club")
+#plt.savefig('Degree_distribution_Karate_Club.pdf')
+#plt.show()
