03dd39536c404b27e0cdd29ae352bbda91d51dbe
Axel Friberg <axl.friberg@gmail.com>
2016-12-07 13:30:40 +0000

    Final version of PageRank script.

--- PageRank.py
+++ PageRank.py
@@ -1,22 +1,29 @@
 import networkx as nx
-import re
+import matplotlib.pyplot as plt
 
-num_edges = 2312497
+num_edges = 5
 current_edges = 0
 
-G=nx.Graph()
-file = open("web-Stanford.txt", 'r')
+G = nx.Graph()
+file = open("ExampleGraph.txt", 'r')
 lines = file.readlines()
-lines = lines[4:] # Remove the instructional lines
 
 print("Adding edges to graph...")
 
 for line in lines:
-    split_line = re.split(r'\t+', line.rstrip('\n'))
-    G.add_edge(split_line[0], split_line[1])
+    split_line = line.split()
+    G.add_edge(int(split_line[0]), int(split_line[1]))
     current_edges += 1
     print("Added " + str(current_edges) + "/" + str(num_edges) + " of total edges.", end="\r")
 
 file.close()
-
 print("\nDone")
+
+pr_dict = nx.pagerank(G)
+print(pr_dict)
+
+plt.bar(range(len(pr_dict)), pr_dict.values(), 0.5, align='center')
+plt.xticks(range(len(pr_dict)), {'A', 'B', 'C', 'D'})
+
+plt.ylim(0, 1)
+plt.savefig("PageRankValues.png")
